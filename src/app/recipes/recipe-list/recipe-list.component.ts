32ecda0f3ee7a89fee00ad0cb53815da9842144c
amityahav90 <33030590+amityahav90@users.noreply.github.com>
2018-08-01 19:08:25 +0300

    Added Directive for dropdown menus

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'this is a test', 'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38030.jpg')
+    new Recipe('Pizza Alfredo', 'Tasty italian pizza!', 'https://torange.biz/photo/38/IMAGE/pizza-health-recipe-38030.jpg'),
+    new Recipe('Baked Salmon Fish', 'Baked salmon with garlic and butter', 'https://c1.staticflickr.com/4/3862/14395796846_f6e5558384_b.jpg')
   ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
